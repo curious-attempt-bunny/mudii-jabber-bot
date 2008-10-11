453a05ed9a38404fb2a788123a00cd09b075e6ee
curious.attempt.bunny <curious.attempt.bunny@0237d960-972a-11dd-8f69-eb63e1080c9a>
2008-10-11 00:35:51 +0000

    Initial import.

---
+++ mudii-jabber-bot/src/com/albery/jabberbot/Main.groovy
@@ -0,0 +1,39 @@
+package com.albery.jabberbot
+import org.jivesoftware.smack.*
+
+class Main {
+	static void main(args) {
+		if (args.length == 3)
+			new Main(user:args[0], password:args[1], partner:args[2]).run()
+		else
+			println "Usage: <user> <password> <partner jabber id>"
+	}
+	
+	def user
+	def password
+	def partner
+	
+	def run() {
+		XMPPConnection connection = new XMPPConnection("gmail.com")
+		
+		connection.connect()
+		connection.login("$user@gmail.com", password)
+		
+		Roster roster = connection.getRoster();
+		roster.addRosterListener([
+            presenceChanged:{presence ->
+		    	println "$presence.from available: $presence.available"
+		    }
+		] as RosterListener)
+
+        roster.reload()
+
+		def chat = connection.getChatManager().createChat(partner, { chat, message ->
+	        println("Received message: " + message.body)
+		} as MessageListener)
+
+		chat.sendMessage("Hi!")
+
+		Thread.sleep(5000);
+	}
+}
